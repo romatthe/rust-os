5d3cc1056324a70112248f3cf93feae3038e81fd
Robin Mattheussen <me@romatthe.dev>
2022-05-19 00:01:44 +0200

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,35 @@
+{
+  description = "Bare metal pure-Rust OS based on Philipp Oppermann's blog posts";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/release-21.11";
+    flake-compat = {
+      url = "github:edolstra/flake-compat";
+      flake = false;
+    };
+    flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, rust-overlay, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+        rust-version = "1.58.1";
+        rust-stable = pkgs.rust-bin.stable.${rust-version}.default.override {
+          extensions = [ "rust-src" ];
+        };
+      in
+      {
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            rust-stable
+            rust-analyzer
+          ];
+        };
+      }
+    );
+}
