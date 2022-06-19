cd2408c6fe336f030676959a913c11f4d3ccde02
Robin Mattheussen <me@romatthe.dev>
2022-06-20 01:34:18 +0200

    interrupts: double fault exception test

---
+++ tests/stack_overflow.rs
@@ -0,0 +1,60 @@
+#![no_std]
+#![no_main]
+#![feature(abi_x86_interrupt)]
+
+use core::panic::PanicInfo;
+use rust_os::{QemuExitCode, exit_qemu, serial_print, serial_println};
+use lazy_static::lazy_static;
+use x86_64::structures::idt::{InterruptDescriptorTable, InterruptStackFrame};
+
+lazy_static! {
+    static ref TEST_IDT: InterruptDescriptorTable = {
+        let mut idt = InterruptDescriptorTable::new();
+
+        unsafe {
+            idt.double_fault
+                .set_handler_fn(test_double_fault_handler)
+                .set_stack_index(rust_os::gdt::DOUBLE_FAULT_IST_INDEX);
+        }
+
+        idt
+    };
+}
+
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    serial_print!("stack_overflow::stack_overflow...\t");
+
+    rust_os::gdt::init();
+    init_test_idt();
+
+    // Trigger a stack overflow
+    stack_overflow();
+
+    panic!("Execution continued after stack overflow");
+}
+
+pub fn init_test_idt() {
+    TEST_IDT.load();
+}
+
+extern "x86-interrupt" fn test_double_fault_handler(frame: InterruptStackFrame, error_code: u64) -> ! {
+    serial_println!("[ok]");
+    exit_qemu(QemuExitCode::Success);
+
+    loop {}
+}
+
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    rust_os::test_panic_handler(info);    
+}
+
+#[allow(unconditional_recursion)]
+fn stack_overflow() {
+    // For each recursion the return address is pushed on the stack
+    stack_overflow();
+    // Prevent tail recursion optimization
+    volatile::Volatile::new(0).read();
+}
+
